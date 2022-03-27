19c45b96f7c0622dae94185c6512fb37d2527de7
guillaume-ventura <venturaguillaume01@gmail.com>
2022-03-27 21:35:53 +0200

    🔧 Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    
+});
+
+Route::middleware('auth:api')->group(function () {
+
 });
